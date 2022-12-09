87240e666dd1f327fa9baf997d2ef3e2fc555be3
Anigirka <dek17@tpu.ru>
2022-12-09 17:02:14 +0700

    Initial commit

---
+++ src/test/java/com/RW/BiologicalProductivity/BiologicalProductivityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.RW.BiologicalProductivity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BiologicalProductivityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
