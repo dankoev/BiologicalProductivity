78fa200033e4ee79fff5834ba9dd19f2580b5895
Anigirka <dek17@tpu.ru>
2022-12-15 23:35:43 +0700

    Create project functions:  division of the data array into sectors and their filling, detected of sectors whithout data, transformation sectrors to heatmaps; reading JSON file with information about map (for example, mapH)

--- src/main/java/com/RW/BiologicalProductivity/services/Calculate.java
+++ src/main/java/com/RW/BiologicalProductivity/services/Calculate.java
@@ -1,23 +1,44 @@
 package com.RW.BiologicalProductivity.services;
 
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+
 import org.opencv.core.Mat;
 import org.opencv.highgui.HighGui;
-import org.opencv.imgcodecs.Imgcodecs;
+
+import com.RW.BiologicalProductivity.services.models.MapInfo;
+import com.google.gson.Gson;
 
 class Calculate {
     static{
         nu.pattern.OpenCV.loadLocally();
         System.out.println("Load library");
     }
-    public static void main(String[] args) {
+    public static String pathToMapHInfo ="mapsInfo/mapHData.json";
 
-        Map map = new Map("dataMaps/H_UTM.tif", Imgcodecs.IMREAD_UNCHANGED | Imgcodecs.IMREAD_ANYDEPTH | Imgcodecs.IMREAD_LOAD_GDAL);
+    public static void main(String[] args) {
+        MapInfo mapHinfo = readJSON(pathToMapHInfo);
+        MapData mapData = new MapData(mapHinfo.pathToMap, mapHinfo.cornerCoords, mapHinfo.maxMapValue, mapHinfo.minMapValue);
+        MapSector  sector = mapData.getFillSector(26);
+
         HighGui.namedWindow("Input", HighGui.WINDOW_AUTOSIZE);
 
-        Mat newMat = map.getGrayMap(206, 1300);
+        Mat newMat = sector.sectorToRGB();
         HighGui.imshow("Input", newMat);
         HighGui.waitKey();
         
+    }
+    public static MapInfo readJSON(String path){
+        try {
+            FileReader jsonString = new FileReader(path);
+            Gson gson = new Gson();
+            MapInfo mapInfo = gson.fromJson(jsonString, MapInfo.class);
+            System.out.println("JSON прочитан для карты: "+ mapInfo.name);
+            return mapInfo;
+        } catch (FileNotFoundException e) { 
+            System.out.println("Проверте наличие файла"+ path);
+            return null;
+        }
     }
     
 }
