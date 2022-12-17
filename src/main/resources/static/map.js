ce00339ab1f66b076d768530e05fd46bb6e13139
Anigirka <dek17@tpu.ru>
2022-12-17 14:03:47 +0700

    Create and add placemarks on map; placemarks coordinates display

--- src/main/resources/static/map.js
+++ src/main/resources/static/map.js
@@ -6,9 +6,43 @@ var mapData = {
 }
 ymaps.ready(init);
 function init(){
+    createMap();
+    addMapEvent();
+    
+}
+function createMap(){
     mapData.map = new ymaps.Map('map', {
         center: mapData.coordinatesCenter,
         zoom: mapData.zoom,
         controls: ['zoomControl', 'typeSelector']
-    });
+    })
+}
+function addMapEvent(){
+    mapData.map.events.add('click', function (events) {
+        let placemark = createPlacemark(events,true)
+        addPlacemarkEvent(placemark,'dragend')
+        mapData.map.geoObjects.add(placemark)
+    })
+}
+function createPlacemark(events,isDraggable){
+    let coords = events.get('coords')
+    let placemark = new ymaps.Placemark(
+        coords,
+        {},
+        {preset: "islands#redIcon",
+            draggable: isDraggable}
+    )
+    showPlacemarkCoords(coords)
+    return placemark
+}
+function addPlacemarkEvent(placemark, action){
+    placemark.events.add(action, function (e) {
+        console.log(this)
+        let newCoord = this.geometry.getCoordinates()
+        showPlacemarkCoords(newCoord)
+    },  placemark)
+}
+function showPlacemarkCoords(coordinates){
+    console.log(coordinates)
+    $('#placemarkCoords').text('Долгота: ' + coordinates[0] + '\nШирота: ' + coordinates[1])
 }
