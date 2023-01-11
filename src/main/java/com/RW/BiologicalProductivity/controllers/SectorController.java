814259c2c6abd25782fd810ae8a4bc35555a5c1b
Anigirka <dek17@tpu.ru>
2023-01-11 21:29:17 +0700

    Adding a countdown to the creation of a sector heatmap

--- src/main/java/com/RW/BiologicalProductivity/controllers/SectorController.java
+++ src/main/java/com/RW/BiologicalProductivity/controllers/SectorController.java
@@ -1,15 +1,27 @@
 package com.RW.BiologicalProductivity.controllers;
 
+
+import java.time.Duration;
+import java.time.Instant;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.RW.BiologicalProductivity.services.ServicesAPI;
 
+
 @RestController
 public class SectorController {
     @GetMapping("/getHeatMap")
     public void getHeatMap() {
-        ServicesAPI.getPathToHeatMap(26, 1);
+        Instant start = Instant.now();
+
+        ServicesAPI.getPathToHeatMap(26, 8);
+
+        Instant finish = Instant.now();
+        long elapsed = Duration.between(start, finish).toMillis();
+        System.out.println("Прошло времени, мс: " + elapsed);
+        
         
     }
     
